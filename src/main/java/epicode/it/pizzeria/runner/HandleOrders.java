b9086e8ba649b6e4e1a53001d1e4b08f1c644bf0
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-12-17 16:26:13 +0100

    add handle order runner

--- src/main/java/epicode/it/pizzeria/runner/HandleOrders.java
+++ src/main/java/epicode/it/pizzeria/runner/HandleOrders.java
@@ -1,5 +1,6 @@
 package epicode.it.pizzeria.runner;
 
+import epicode.it.pizzeria.entity.order.Status;
 import epicode.it.pizzeria.repository.OrderRepo;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -21,13 +22,46 @@ public class HandleOrders implements ApplicationRunner {
     @Transactional
     public void run(ApplicationArguments args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        List<epicode.it.pizzeria.entity.order.Order> orders = orderRepo.findAll();
-        while (true) {
-            System.out.println("List of orders:");
-            orders.forEach(System.out::println);
-            System.out.println("Select order to manage:");
-
-
+        try {
+            while (true) {
+                List<epicode.it.pizzeria.entity.order.Order> orders = orderRepo.findAll();
+                System.out.println("List of orders:");
+                orders.forEach(System.out::println);
+                System.out.println("Select order to manage:");
+                long opt = scanner.nextLong();
+                scanner.nextLine();
+                if(opt == 0) return;
+                if (opt <= orders.size()) {
+                    epicode.it.pizzeria.entity.order.Order selectedOrder = orderRepo.findById(opt).orElse(null);
+                    boolean valid = false;
+                    while (!valid) {
+                        System.out.println("Select new order status: 1-IN_PREPARATION 2-DELIVERED");
+                        int status = scanner.nextInt();
+                        scanner.nextLine();
+                        switch (status) {
+                            case 1 -> {
+                                selectedOrder.setStatus(Status.IN_PREPARATION);
+                                orderRepo.save(selectedOrder);
+                                valid = true;
+                            }
+                            case 2 -> {
+                                selectedOrder.setStatus(Status.DELIVERED);
+                                orderRepo.save(selectedOrder);
+                                valid = true;
+                            }
+                            default -> {
+                                System.out.println("Selection is not valid, retry");
+                            }
+                        }
+                    }
+                } else if(opt > orders.size()){
+                    System.out.println("Invalid selection, retry");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            scanner.close();
         }
     }
 }
